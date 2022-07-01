7acaf58cb506fc5713a63e1d79af63305d9a8696
Elyas Santana <picpay@AGM-609865.local>
2022-06-30 23:09:35 -0300

    Atividade de Mini API com express

--- atividade01_elyas/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var input = require("readline-sync");
-var nome = input.question('Digite o nome: ');
-var idade;
-idade = parseInt(input.question('Digite a idade: '));
-console.log('Olá mundo de Devs!!!');
-console.log('O nome digitado foi: ', nome);
-console.log('A idade digitada foi: ', idade);
